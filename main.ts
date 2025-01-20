b65f6503abebccd0787af2d43b3f6627bb801566
Alex Katopodis <akatopo@users.noreply.github.com>
2025-01-21 00:28:51 +0300

    ✨ add initial plugin source

--- main.ts
+++ main.ts
@@ -1,134 +1,69 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { Plugin, Notice } from "obsidian";
+import { BrowserWindow } from "electron";
 
-// Remember to rename these classes and interfaces!
+const className = "sidebar-vibrancy";
 
-interface MyPluginSettings {
-	mySetting: string;
+const setVibrancyFailedMsg =
+	"Failed to updated the vibrancy value, are you on the right platform?";
+
+function hasTranslucentClass() {
+	return document.body.hasClass("is-translucent");
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+function setVibrancy(
+	electronWindow: Electron.BrowserWindow | undefined,
+	vibrancy: "under-window" | null = "under-window"
+) {
+	const { platform } = globalThis.process;
+	if (!electronWindow || platform !== "darwin") {
+		return false;
+	}
+
+	electronWindow.setBackgroundColor("#00000000");
+	if (!hasTranslucentClass()) {
+		electronWindow.setVibrancy(vibrancy);
+	}
+	return true;
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+function enable(electronWindow: Electron.BrowserWindow | undefined) {
+	if (!setVibrancy(electronWindow)) {
+		new Notice(setVibrancyFailedMsg);
+		throw new Error(setVibrancyFailedMsg);
+	}
+	document.body.addClass(className);
+}
 
+function disable(electronWindow: Electron.BrowserWindow | undefined) {
+	if (!setVibrancy(electronWindow, null)) {
+		return;
+	}
+	document.body.removeClass(className);
+}
+
+function checkForTranslucentWindowOpt() {
+	if (!hasTranslucentClass()) {
+		return;
+	}
+	const msg =
+		'The "Translucent window" option appears to be enabled, please disable it and restart the plugin';
+	new Notice(msg);
+	throw new Error(msg);
+}
+
+export default class SidebarVibrancy extends Plugin {
+	private electronWindow?: BrowserWindow;
 	async onload() {
-		await this.loadSettings();
+		checkForTranslucentWindowOpt();
+		this.electronWindow = globalThis
+			.require("electron")
+			// @ts-ignore
+			.remote.getCurrentWindow() as BrowserWindow;
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+		enable(this.electronWindow);
 	}
 
 	onunload() {
-
-	}
-
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
-
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
-}
-
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
-
-	display(): void {
-		const {containerEl} = this;
-
-		containerEl.empty();
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
+		disable(this.electronWindow);
 	}
 }
